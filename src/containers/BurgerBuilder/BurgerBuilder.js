34c54e6dc295d5ace5151aa132657d90a9fd635e
Jordan <jszins@gmail.com>
2018-08-08 15:44:57 -0500

    buttons on modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,9 @@ class BurgerBuilder extends Component {
   purchaseCancelHandler = () => {
     this.setState({purchasing: false})
   }
+  purchaseContinueHandler = () => {
+    alert('You Continue!')
+  }
 
   render () {
     const disabledInfo ={
@@ -88,7 +91,11 @@ class BurgerBuilder extends Component {
         <Modal
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancel={this.purchaseCancelHandler}
+            purchaseContinue={this.purchaseContinueHandler}
+                 />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
