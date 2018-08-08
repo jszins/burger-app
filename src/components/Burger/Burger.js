d05edbba1175549cda3e062e795ca5c4bee0012c
Jordan <jszins@gmail.com>
2018-08-08 12:49:13 -0500

    generic method to output ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,16 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />
+      });
+    });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="cheese" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   )
