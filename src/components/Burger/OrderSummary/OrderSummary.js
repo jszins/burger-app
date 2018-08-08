b4518bb78fbc4f8c31e520dcccec0f30c1fd7bd5
Jordan <jszins@gmail.com>
2018-08-08 15:16:28 -0500

    order summary component functional

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Aux from '../../../hoc/Aux'
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return <li key={ingKey}>
+                <span
+                  style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+              </li>
+    })
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>Big Borgar with:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Continue to Checkout?</p>
+    </Aux>
+  )
+}
+
+export default orderSummary
