abbb13b4ca5b253a3b95b5ab75f434b679326d3e
Jordan <jszins@gmail.com>
2018-08-08 15:33:30 -0500

    backdrop on modal working

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Backdrop.css'
 
 const backdrop = (props) => {
-  props.show ? <div className={classes.Backdrop}></div> : null
+  return props.show ? <div className={classes.Backdrop} onClick={props.backdrop}></div> : null
 }
 
 export default backdrop
