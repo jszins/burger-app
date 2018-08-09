314e175a6a9561406328e95fa3e9835529f00a67
Jordan <jszins@gmail.com>
2018-08-09 08:59:22 -0500

    turn layout into functional component

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 import classes from './SideDrawer.css'
 
 const sideDrawer = (props) => {
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="11%"/>
-      <nav >
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop show/>
+      <div className={classes.SideDrawer}>
+        <Logo height="11%"/>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   )
 }
 
