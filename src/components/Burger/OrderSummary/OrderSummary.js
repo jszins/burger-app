f797e486b356b0f6568eabc31b860c7d94d9c75e
Jordan <jszins@gmail.com>
2018-08-09 10:40:21 -0500

    components to classes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(ingKey => {
-      return <li key={ingKey}>
-                <span
-                  style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
-              </li>
+      return (
+        <li key={ingKey}>
+          <span
+            style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+        </li>
+      )
     })
-  return (
-    <Aux>
-      <h3>Your order</h3>
-      <p>Big Borgar with:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancel}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-    </Aux>
-  )
+    return (
+      <Aux>
+        <h3>Your order</h3>
+        <p>Big Borgar with:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancel}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary
+export default OrderSummary
